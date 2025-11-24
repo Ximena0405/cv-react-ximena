896653f4e1599a6182b0d69e2f293e6be3e7f236
Ximena Roldán García <ximenaroldan367@gmail.com>
2025-11-24 10:38:06 -0500

    refactor(estudios): actualizar componente Estudios.jsx

--- src/Estudios.jsx
+++ src/Estudios.jsx
@@ -1,26 +1,15 @@
-import "./Estudios.css"
-export default function Estudios() {
-    return(
-    <section className="estudios-container">
-      <div className="estudios-card">
-        <h1 className="estudios-titulo">Formación Académica</h1>
+export default function Estudios({ estudios }) {
+  return (
+    <section className="est-container">
+      <h2 className="est-title">Estudios</h2>
 
-        {/* Bachiller */}
-        <div className="estudio-item">
-          <h2 className="estudio-tipo">Técnico</h2>
-          <p className="estudio-institucion">
-            Análisis y Desarrollo de Software
-          </p>
-          <p className="estudio-fecha">2022 - 2023</p>
+      {estudios.map((item, i) => (
+        <div className="est-card" key={i}>
+          <h3>{item.titulo}</h3>
+          <p>{item.institucion}</p>
+          <p>{item.fecha}</p>
         </div>
-
-        {/* Tecnólogo */}
-        <div className="estudio-item">
-          <h2 className="estudio-tipo">Tecnólogo</h2>
-          <p className="estudio-institucion">Servicio Nacional De Aprendizaje (SENA)</p>
-          <p className="estudio-fecha">29/04/2025 - Actualidad</p>
-        </div>
-      </div>
+      ))}
     </section>
-    )
+  );
 }
