0bf3f93f9c7c99e50dbc3dc2860289562764b15f
Ximena Roldán García <ximenaroldan367@gmail.com>
2025-11-24 10:25:39 -0500

    refactor(app): ajustar componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+import { cvData } from "./data/cvData";
+
 import Cabecera from "./Cabecera";
 import Perfil from "./Perfil";
 import Experiencia from "./Experiencia";
 import Estudios from "./Estudios";
-import Tecnologias from "./StackTecnologias"; 
+import Tecnologias from "./StackTecnologias";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-      <Cabecera />
-      <Perfil />
-      <Experiencia />
-      <Estudios />
-      <Tecnologias />
+      {/* Cabecera principal */}
+      <Cabecera {...cvData.cabecera} />
+
+      {/* Sección de perfil */}
+      <Perfil 
+        descripcion={cvData.perfil.descripcion} 
+        aptitudes={cvData.perfil.aptitudes} 
+        hobbies={cvData.perfil.hobbies} 
+      />
+
+      {/* Experiencia laboral */}
+      <Experiencia experiencia={cvData.experiencia} />
+
+      {/* Estudios */}
+      <Estudios estudios={cvData.estudios} />
+
+      {/* Stack tecnológico */}
+      <Tecnologias tecnologias={cvData.tecnologias} />
     </div>
   );
 }
