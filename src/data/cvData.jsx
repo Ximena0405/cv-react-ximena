97489dca18775fe97f799c72048284e144c46d45
Ximena Roldán García <ximenaroldan367@gmail.com>
2025-11-24 10:23:47 -0500

    feat: creacion de carpeta data y archivo cvData.jsx

---
+++ src/data/cvData.jsx
@@ -0,0 +1,59 @@
+export const cvData = {
+  cabecera: {
+    nombre: "Ximena Roldán García",
+    profesion: "Estudiante de Programación",
+    correo: "ximenaroldan367@gmail.com",
+    telefono: "3150896523",
+    ubicacion: "Calle 37 #58 B-17",
+    foto: "public/img/perfil.jpeg"
+  },
+
+  perfil: {
+    descripcion:
+      "Soy una estudiante de programación con interés en desarrollo web, diseño limpio y buenas prácticas. Me gusta aprender nuevas tecnologías y aplicarlas en proyectos personales.",
+    aptitudes: ["Trabajo en equipo", "Resolución de problemas", "Aprendizaje rápido", "Creatividad", "Pensamiento lógico"],
+    hobbies: ["Aprender tecnologías", "Diseño web", "Documentar proyectos", "Música", "Lectura"]
+  },
+
+  experiencia: [
+    {
+      cargo: "Asistente de Desarrollo Web",
+      fecha: "2024",
+      descripcion: "Apoyo básico en maquetación HTML y estilos con CSS."
+    },
+    {
+      cargo: "Practicante de Desarrollo Frontend",
+      fecha: "2024",
+      descripcion: "Tareas simples en React: componentes, props y estados."
+    },
+    {
+      cargo: "Proyecto personal JavaScript",
+      fecha: "2025",
+      descripcion: "Creación de pequeñas funciones, validaciones y lógica."
+    },
+    {
+      cargo: "Diseño UI básico",
+      fecha: "2025",
+      descripcion: "Creación de interfaces limpias con Figma y CSS."
+    }
+  ],
+
+  estudios: [
+    {
+      titulo: "Tecnólogo en Análisis y Desarrollo de Software",
+      institucion: "SENA",
+      fecha: "2024 - Actual"
+    },
+    {
+      titulo: "Curso de JavaScript Básico",
+      institucion: "Platzi",
+      fecha: "2024"
+    }
+  ],
+
+  tecnologias: {
+    Lenguajes: ["HTML5", "CSS", "JavaScript", "Python", "PHP", "Node.js"],
+    Frameworks: ["React", "Express", "Tailwind CSS"],
+    Herramientas: ["Git & GitHub", "Visual Studio Code", "MySQL", "MongoDB"]
+  }
+};
